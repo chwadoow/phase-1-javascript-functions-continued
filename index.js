1de596ee519e23a19c404d03ed8914be76bf669a
moses ngige <moses32ngigz@gmail.com>
2022-06-08 10:22:26 +0300

    commit tree 77146036ef5b0d72eb8fceb7e50a575c5eb4e1c1

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun (game = "roller-skate") {
+    return (`This Saturday, I want to ${game}!`)
+
+}
+function mondayWork(tame = 'go to the office'){
+    return `This Monday, I will ${tame}.`;
+}
+
+function wrapAdjective (string){
+    return function (grimm ='special') {
+        return `You are ${string}${grimm}${string}!`;}
+
+    };
+    
+
+
